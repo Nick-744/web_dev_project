091bdfee78c5c0d82b5f8f68f459152614a1e2fb
ManyaZ1 <96390140+ManyaZ1@users.noreply.github.com>
2025-05-11 10:59:30 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ app.use((req, res, next) => {
     res.set('Cache-Control', 'no-store');
     next();
 });
-app.use((req, res, next) => {
-    console.log(`Incoming request: ${req.method} ${req.url}`);
-    next();
-});
 // app.use((req, res, next) => {
 //     console.log(`Incoming request: ${req.method} ${req.url}`);
 //     next();
